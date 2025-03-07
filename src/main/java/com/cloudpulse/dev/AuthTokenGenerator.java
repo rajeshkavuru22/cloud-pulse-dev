6c106200c4e754c3c8259887fc5553cd25d09406
rajeshkavuru22 <rajeshkavuru76@gmail.com>
2025-03-07 15:26:44 +0530

    FileStream Updated

--- src/main/java/com/cloudpulse/dev/AuthTokenGenerator.java
+++ src/main/java/com/cloudpulse/dev/AuthTokenGenerator.java
@@ -1,26 +1,45 @@
 package com.cloudpulse.dev;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.auth.oauth2.AccessToken;
+import org.springframework.core.io.ClassPathResource;
+
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ClassPathResource;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.util.Collections;
 
 public class AuthTokenGenerator {
 
     public static String getAccessToken() throws IOException {
-        // Loading the service account key JSON file
-        FileInputStream serviceAccountStream = new FileInputStream("C:\\Cloud Pulse\\dev\\src\\main\\resources\\service-account-data-jsonfile.json");
+        try {
+            Resource resource = new ClassPathResource("service-account-data-jsonfile.json");
 
-        // Authenticate using GoogleCredentials
-        GoogleCredentials credentials = GoogleCredentials.fromStream(serviceAccountStream)
-                .createScoped(Collections.singletonList("https://www.googleapis.com/auth/cloud-platform"));
+            if (resource.exists()) {
+                // Get InputStream from the resource
+                InputStream inputStream = resource.getInputStream();
 
-        // Refresh token
-        credentials.refreshIfExpired();
-        AccessToken token = credentials.getAccessToken();
+                //FileInputStream serviceAccountStream = new FileInputStream();
 
-        return token.getTokenValue();
+                // Authenticate using GoogleCredentials
+                GoogleCredentials credentials = GoogleCredentials.fromStream(inputStream)
+                        .createScoped(Collections.singletonList("https://www.googleapis.com/auth/cloud-platform"));
+
+                // Refresh token
+                credentials.refreshIfExpired();
+                AccessToken token = credentials.getAccessToken();
+
+                return token.getTokenValue();
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return null;
     }
 
     public static void main(String[] args) throws IOException {
